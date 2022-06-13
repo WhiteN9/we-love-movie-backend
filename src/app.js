480ef3765c0b623611bf0cd9bd6f5f237c2dda63
White Nguyen <whitenguyen0909@gmail.com>
2022-06-13 09:43:35 -0600

    set up error handlers for not found route or not allowed route

--- src/app.js
+++ src/app.js
@@ -1,5 +1,16 @@
 if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
+const notFound = require("./errors/notFound");
+const errorHandler = require("./errors/errorHandler");
+
+app.use(express.json());
+
+module.exports = app;
+
+//App routers
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
