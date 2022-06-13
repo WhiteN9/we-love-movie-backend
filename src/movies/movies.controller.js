a0ab515162c330908b8db82469570770d036b3ce
White Nguyen <whitenguyen0909@gmail.com>
2022-06-13 14:17:43 -0600

    added function to list theaters by movie id, or to list reviews by movie id

--- src/movies/movies.controller.js
+++ src/movies/movies.controller.js
@@ -4,14 +4,48 @@ async function list(req, res) {
   //   console.log(req.query.is_showing);
   const is_showing = req.query.is_showing;
   if (is_showing) {
-    const movieList = await service.listShowingMovies();
+    const moviesList = await service.listShowingMovies();
     // console.log(movieList);
-    res.json({ data: movieList });
+    res.json({ data: moviesList });
   } else {
-    const movieList = await service.list();
-    res.json({ data: movieList });
+    const moviesList = await service.list();
+    res.json({ data: moviesList });
   }
 }
+
+async function read(req, res) {
+  // console.log(res.locals.movie)
+  res.json({ data: res.locals.movie });
+}
+
+async function listTheatersByMovie(req, res) {
+  const theatersList = await service.listTheatersByMovie(req.params.movieId);
+  // console.log(theatersList);
+  res.json({ data: theatersList });
+}
+
+async function listReviewsByMovie(req, res) {
+  const reviewsList = await service.listReviewsByMovie(req.params.movieId);
+  // console.log(reviewsList);
+  res.json({ data: reviewsList });
+}
+
+/////// MIDDLEWARE //////////
+
+async function movieExists(req, res, next) {
+  // console.log(req.params.movieId);
+  const movie = await service.read(req.params.movieId);
+
+  if (movie) {
+    res.locals.movie = movie;
+    return next();
+  }
+  next({ status: 404, message: `Movie cannot be found.` });
+}
+
 module.exports = {
   list,
+  read: [movieExists, read],
+  listTheatersByMovie: [movieExists, listTheatersByMovie],
+  listReviewsByMovie: [movieExists, listReviewsByMovie],
 };
--- src/movies/movies.service.js
+++ src/movies/movies.service.js
@@ -5,14 +5,40 @@ function list() {
 }
 
 function listShowingMovies() {
+  return (
+    knex("movies AS m")
+      .join("movies_theaters AS mt", "mt.movie_id", "=", "m.movie_id")
+      //might need to select matching column with groupBy depend on PostgreSQL
+      .select("*")
+      .where({ "mt.is_showing": true })
+      .groupBy("mt.movie_id")
+  );
+}
+
+function read(movie_id) {
+  return knex("movies").select("*").where({ movie_id }).first();
+}
+
+function listTheatersByMovie(movie_id) {
   return knex("movies AS m")
     .join("movies_theaters AS mt", "mt.movie_id", "=", "m.movie_id")
-
+    .join("theaters AS t", "t.theater_id", "=", "mt.theater_id")
     .select("*")
-    .where({ is_showing: true });
+    .where({ "mt.movie_id": movie_id });
+}
+
+function listReviewsByMovie(movie_id) {
+  return knex("movies AS m")
+    .join("reviews AS r", "r.movie_id", "=", "m.movie_id")
+    .join("critics AS c", "c.critic_id", "=", "r.critic_id")
+    .select("r.*", "c.organization_name", "c.preferred_name", "c.surname")
+    .where({ "r.movie_id": movie_id });
 }
 
 module.exports = {
   list,
   listShowingMovies,
+  read,
+  listTheatersByMovie,
+  listReviewsByMovie,
 };
